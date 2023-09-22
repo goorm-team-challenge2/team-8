df4a4ff83055f3cc1693730d36e1c631f6468576
luke-sonn <luke.son@goorm.io>
2023-09-22 20:13:12 +0900

    fix: Card Component 업데이트

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,14 +1,18 @@
 import cn from 'classnames';
 
+import { Typography } from '@goorm-dev/gds-challenge';
+
 import styles from './Card.module.scss';
 
 /**
  * @typedef CardProps
- * @property {'white' | 'gray-50'} [backgroundColor]
+ * @property {'content'} [type]
  * @property {'top' | 'bottom' | 'both'} [flat]
  * @property {'none' | 'md'} [padding]
  * @property {boolean} [pointer=false]
  * @property {boolean} [center=false]
+ * @property {string} [title]
+ * @property {boolean} [between=false]
  */
 
 /**
@@ -16,32 +20,56 @@ import styles from './Card.module.scss';
  * @returns
  */
 const Card = ({
-	backgroundColor = 'white',
-	flat,
+	type,
+	flat = type === 'content' && 'both',
 	padding = 'md',
-	pointer = false,
+	pointer = type === 'toggle',
 	center = false,
+	title,
+	between = false,
 	className,
+	children,
 	...props
 }) => {
-	return (
-		<div
-			className={cn(
-				styles.card,
-				{
-					[styles[`backgroundColor_${backgroundColor}`]]:
-						!!backgroundColor,
-					[styles[`flat_${flat}`]]: !!flat,
-					[styles[`padding_${padding}`]]:
-						!!padding && padding !== 'none',
-					[styles.pointer]: !!pointer,
-					[styles.center]: !!center,
-				},
-				className,
-			)}
-			{...props}
-		></div>
-	);
+	const commonClassName = cn(styles.card, between && styles.between, {
+		[styles[`flat_${flat}`]]: !!flat,
+		[styles[`padding_${padding}`]]: !!padding && padding !== 'none',
+		[styles.pointer]: !!pointer,
+		[styles.center]: !!center,
+	});
+
+	switch (type) {
+		case 'content':
+			return (
+				<div
+					className={cn(
+						styles.card_content,
+						styles['backgroundColor_gray-50'],
+						commonClassName,
+						className,
+					)}
+					{...props}
+				>
+					<Typography color="hint" className={styles.title}>
+						{title}
+					</Typography>
+					<div className={styles.content}>{children}</div>
+				</div>
+			);
+		default:
+			return (
+				<div
+					className={cn(
+						styles.backgroundColor_white,
+						commonClassName,
+						className,
+					)}
+					{...props}
+				>
+					{children}
+				</div>
+			);
+	}
 };
 
 export default Card;

41d14a01fdeefe42399f8de03d580cd9e91f9bd6
ppparkta <pppppark2@gmail.com>
2023-09-23 16:46:25 +0900

    feat: 페이지 연결 및 로컬스토리지 저장

--- src/components/Survey/Step4/Step4.jsx
+++ src/components/Survey/Step4/Step4.jsx
@@ -8,10 +8,21 @@ import {
 import styles from '../Font.module.css';
 
 export default function Step4(props) {
-	const { survey, setSurvey, setCurrentScreen } = props;
+	const { survey, setIsOpen, setSurvey, setCurrentScreen } = props;
 
 	const handlePrev = () => {
 		setCurrentScreen(1);
+	};
+
+	const handleSubmit=()=>{
+		localStorage.setItem('surveyData', JSON.stringify(survey));
+		setCurrentScreen(0);
+		setIsOpen(false);
+	};
+
+	const handleSurvey4Change = (e) => {
+		const value = e.target.value;
+		setSurvey((prevSurvey) => ({ ...prevSurvey, survey4: value }));
 	};
 	
 	return (
@@ -28,6 +39,8 @@ export default function Step4(props) {
 					resize="vertical"
 					rows={4}
 					block
+					value={survey.survey4}
+					onChange={handleSurvey4Change}
 					placeholder="ex.
 다음 번 챌린지에서는 더 어려운 문제가 출제되면 좋겠어요.
 오프라인 과제가 다양했으면 좋겠어요.
@@ -43,7 +56,7 @@ export default function Step4(props) {
 					<Button size="lg" color="link" onClick={handlePrev}>
 						이전
 					</Button>
-					<Button size="lg">제출하기</Button>
+					<Button size="lg" onClick={handleSubmit}>제출하기</Button>
 				</div>
 			</Modal.Footer>
 		</>

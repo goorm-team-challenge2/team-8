dcfb54932ffd28989af239bdad0c0dc0437e373c
ppparkta <pppppark2@gmail.com>
2023-09-23 16:17:03 +0900

    feat: modal 1번, 4번 추가 및 state 생성하여 페이지  연결

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,32 @@
 import cn from 'classnames';
+import Step1 from '../Survey/Step1/Step1';
+import Step4 from '../Survey/Step4/Step4';
 
 import { Button, Typography, Modal } from '@goorm-dev/gds-challenge';
 
 import styles from './Header.module.scss';
 import { useState } from 'react';
 
+const initialSurvey = {
+	username: "",
+	phone: "",
+	email: "",
+	survey1: "",
+	survey2: "",
+	survey2_1: "",
+	survey2_2: "",
+	survey3: "",
+	survey4: ""
+  };
+
+
 const Header = () => {
 	const [isOpen, setIsOpen] = useState(false);
+	const [currentScreen, setCurrentScreen] = useState(1);
+	const [survey, setSurvey] = useState({ ...initialSurvey });
 
 	function toggle(){
-		setIsOpen(isOpen==true?false:true);
+		setIsOpen(isOpen==true?(false, setCurrentScreen(1)):true);
 	};
 
 	return (
@@ -21,18 +38,9 @@ const Header = () => {
 				<Button size="lg" onClick={toggle} >설문조사 참여하기</Button>
 				<>
 					<Modal isOpen={isOpen} toggle={toggle}>
-						<Modal.Header toggle={toggle}>
-							헤더
-						</Modal.Header>
-						<Modal.Body>
-							컨텐츠를 넣어주세요.
-						</Modal.Body>
-						<Modal.Footer>
-							<Button size="lg" color="link">이전</Button>
-							<Button size="lg">다음</Button>
-						</Modal.Footer>
+						{currentScreen == 1 && <Step1 survey={survey} setSurvey={setSurvey} setCurrentScreen={setCurrentScreen}></Step1>}
+						{currentScreen == 4 && <Step4 survey={survey} setSurvey={setSurvey} setCurrentScreen={setCurrentScreen}></Step4>}
 					</Modal>
-				
 				</>
 			</div>
 		</header>
---
+++ src/components/Survey/Step4/Step4.jsx
@@ -0,0 +1,51 @@
+import {
+	Modal,
+	TextArea,
+	CarouselIndicators,
+	Button,
+} from '@goorm-dev/gds-challenge';
+
+import styles from '../Font.module.css';
+
+export default function Step4(props) {
+	const { survey, setSurvey, setCurrentScreen } = props;
+
+	const handlePrev = () => {
+		setCurrentScreen(1);
+	};
+	
+	return (
+		<>
+			<Modal.Header>
+				<span className={styles.font1}>
+					구름톤 챌린지에 <br />
+					전하고 싶은 말을 적어주세요.
+				</span>
+			</Modal.Header>
+			<Modal.Body>
+				더 좋은 챌린지가 될 수 있도록 데이터를 수집하려고 해요.
+				<TextArea
+					resize="vertical"
+					rows={4}
+					block
+					placeholder="ex.
+다음 번 챌린지에서는 더 어려운 문제가 출제되면 좋겠어요.
+오프라인 과제가 다양했으면 좋겠어요.
+"
+				></TextArea>
+			</Modal.Body>
+			<Modal.Footer>
+				<CarouselIndicators
+					length={4}
+					activeIndex={3}
+				></CarouselIndicators>
+				<div>
+					<Button size="lg" color="link" onClick={handlePrev}>
+						이전
+					</Button>
+					<Button size="lg">제출하기</Button>
+				</div>
+			</Modal.Footer>
+		</>
+	);
+}
